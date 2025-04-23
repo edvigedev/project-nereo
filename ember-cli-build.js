bf833ea09ea0f48c61002042486a1f73258a9824
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-04-23 14:48:14 +0200

    feat(nasa-missions.css) create the cards container and adjust the search component

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {});
 
-  app.import('app/styles/homepage.css');
-  app.import('app/styles/nasa-missions.css');
+  // app.import('app/styles/homepage.css');
+  // app.import('app/styles/nasa-missions.css');
 
   return app.toTree();
 };
