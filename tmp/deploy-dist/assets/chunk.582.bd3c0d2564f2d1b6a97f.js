ae4664e2b871c0f4bb68a3a8e03e46ac4b8e361b
Edvige Di Salvo <edvige.disalvo@qonto.com>
2025-04-30 10:29:48 +0200

    fix (app.css) adjust css styles of logo and page title

--- tmp/deploy-dist/assets/chunk.582.bd3c0d2564f2d1b6a97f.js
+++ tmp/deploy-dist/assets/chunk.582.bd3c0d2564f2d1b6a97f.js
@@ -2,24 +2,24 @@ var __ember_auto_import__;(()=>{var e,r={104:e=>{"use strict"
 e.exports=require("@ember/object/proxy")},123:e=>{"use strict"
 e.exports=require("@ember/object/observers")},217:e=>{"use strict"
 e.exports=require("@glimmer/tracking/primitives/cache")},223:e=>{"use strict"
-e.exports=require("@ember/runloop")},280:e=>{"use strict"
+e.exports=require("@ember/runloop")},271:(e,r,t)=>{e.exports=function(){_eai_d
+var e=_eai_r
+window.emberAutoImportDynamic=function(r){return 1===arguments.length?e("_eai_dyn_"+r):e("_eai_dynt_"+r)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(r){return e("_eai_sync_"+r)(Array.prototype.slice.call(arguments,1))},t(417)}()},280:e=>{"use strict"
 e.exports=require("@ember/object/promise-proxy-mixin")},294:e=>{"use strict"
 e.exports=require("@ember/application")},336:e=>{"use strict"
-e.exports=require("@ember/component/helper")},389:e=>{"use strict"
+e.exports=require("@ember/component/helper")},364:function(e,r){window._eai_r=require,window._eai_d=define},389:e=>{"use strict"
 e.exports=require("@ember/array")},394:e=>{"use strict"
 e.exports=require("@ember/object/compat")},410:e=>{"use strict"
-e.exports=require("@ember/array/proxy")},428:(e,r,t)=>{e.exports=function(){var e=_eai_d,r=_eai_r
+e.exports=require("@ember/array/proxy")},417:(e,r,t)=>{e.exports=function(){var e=_eai_d,r=_eai_r
 function o(e){return e&&e.__esModule?e:Object.assign({default:e},e)}window.emberAutoImportDynamic=function(e){return 1===arguments.length?r("_eai_dyn_"+e):r("_eai_dynt_"+e)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(e){return r("_eai_sync_"+e)(Array.prototype.slice.call(arguments,1))},e("@ember-data/debug/data-adapter",EAI_DISCOVERED_EXTERNALS("@ember-data/debug/data-adapter"),(function(){return o(t(175))})),e("@ember-data/request-utils/deprecation-support",EAI_DISCOVERED_EXTERNALS("@ember-data/request-utils/deprecation-support"),(function(){return o(t(239))})),e("@ember-data/serializer/transform",EAI_DISCOVERED_EXTERNALS("@ember-data/serializer/transform"),(function(){return o(t(414))})),e("@ember/string",EAI_DISCOVERED_EXTERNALS("@ember/string"),(function(){return o(t(871))})),e("@fortawesome/ember-fontawesome/components/fa-icon",EAI_DISCOVERED_EXTERNALS("@fortawesome/ember-fontawesome/components/fa-icon"),(function(){return o(t(662))})),e("@fortawesome/fontawesome-svg-core",EAI_DISCOVERED_EXTERNALS("@fortawesome/fontawesome-svg-core"),(function(){return o(t(957))})),e("@fortawesome/free-solid-svg-icons",EAI_DISCOVERED_EXTERNALS("@fortawesome/free-solid-svg-icons"),(function(){return o(t(551))})),e("ember-data/store",EAI_DISCOVERED_EXTERNALS("ember-data/store"),(function(){return o(t(534))})),e("ember-load-initializers",EAI_DISCOVERED_EXTERNALS("ember-load-initializers"),(function(){return o(t(152))})),e("ember-page-title/helpers/page-title",EAI_DISCOVERED_EXTERNALS("ember-page-title/helpers/page-title"),(function(){return o(t(665))})),e("ember-page-title/services/page-title",EAI_DISCOVERED_EXTERNALS("ember-page-title/services/page-title"),(function(){return o(t(458))})),e("ember-resolver",EAI_DISCOVERED_EXTERNALS("ember-resolver"),(function(){return o(t(923))})),e("leaflet",EAI_DISCOVERED_EXTERNALS("leaflet"),(function(){return o(t(55))}))}()},463:e=>{"use strict"
 e.exports=require("@ember/-internals/metal")},465:e=>{"use strict"
 e.exports=require("@ember/template-factory")},471:e=>{"use strict"
 e.exports=require("@ember/object")},553:e=>{"use strict"
 e.exports=require("@ember/utils")},603:e=>{"use strict"
 e.exports=require("@ember/debug")},606:e=>{"use strict"
-e.exports=require("@glimmer/validator")},625:function(e,r){window._eai_r=require,window._eai_d=define},663:e=>{"use strict"
+e.exports=require("@glimmer/validator")},663:e=>{"use strict"
 e.exports=require("@ember/component")},666:e=>{"use strict"
-e.exports=require("@ember/object/internals")},678:(e,r,t)=>{e.exports=function(){_eai_d
-var e=_eai_r
-window.emberAutoImportDynamic=function(r){return 1===arguments.length?e("_eai_dyn_"+r):e("_eai_dynt_"+r)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(r){return e("_eai_sync_"+r)(Array.prototype.slice.call(arguments,1))},t(428)}()},712:e=>{"use strict"
+e.exports=require("@ember/object/internals")},712:e=>{"use strict"
 e.exports=require("@ember/helper")},735:e=>{"use strict"
 e.exports=require("@ember/service")},806:e=>{"use strict"
 e.exports=require("@ember/debug/data-adapter")},855:e=>{"use strict"
@@ -42,6 +42,6 @@ var r=(r,t)=>{var i,s,[n,a,u]=t,c=0
 if(n.some((r=>0!==e[r]))){for(i in a)o.o(a,i)&&(o.m[i]=a[i])
 if(u)var p=u(o)}for(r&&r(t);c<n.length;c++)s=n[c],o.o(e,s)&&e[s]&&e[s][0](),e[s]=0
 return o.O(p)},t=globalThis.webpackChunk_ember_auto_import_=globalThis.webpackChunk_ember_auto_import_||[]
-t.forEach(r.bind(null,0)),t.push=r.bind(null,t.push.bind(t))})(),o.O(void 0,[562],(()=>o(625)))
-var i=o.O(void 0,[562],(()=>o(678)))
+t.forEach(r.bind(null,0)),t.push=r.bind(null,t.push.bind(t))})(),o.O(void 0,[562],(()=>o(364)))
+var i=o.O(void 0,[562],(()=>o(271)))
 i=o.O(i),__ember_auto_import__=i})()
